b44278df1b72f06dd8e4cb255936f92bd3cc9b55
Atharv Bhosekar <abhosekar@gams.com>
2023-09-14 16:36:08 -0400

    clean up commented code

--- gamstransfer/src/Write.cpp
+++ gamstransfer/src/Write.cpp
@@ -350,8 +350,6 @@ bool compress, int mode) {
     // get default values
     if (varType != GMS_DT_SET) {
       List default_values = symname["defaultValues"];
-      // Function default_val_fun = symname[".getDefaultValues"];
-      // List default_values = default_val_fun();
       for (int d = 0; d < n_attr; d++) {
          mysym_info.default_values[d] = default_values[d];
       }
