6dc3f62675a668e9c9fafe6e06dd4347c075f326
Atharv Bhosekar <abhosekar@gams.com>
2024-02-22 10:46:30 -0500

    keep gdx LICENSE file

--- update_buildignore.R
+++ update_buildignore.R
@@ -4,6 +4,8 @@ gdx_ignore = gdx_ignore[gdx_ignore != "src/gdx/src"]
 
 gdx_src_all = list.files("src/gdx/src", full.names=TRUE)
 gdx_src_cpp_h = list.files("src/gdx/src", pattern="(*\\.cpp$)|(*\\.h)", full.names=TRUE)
+gdx_license = "src/gdx/LICENSE"
 ignore_list = setdiff(gdx_src_all, gdx_src_cpp_h)
+ignore_list = setdiff(ignore_list, gdx_license)
 ignore_list = append(gdx_ignore, ignore_list)
 usethis::use_build_ignore(ignore_list)
