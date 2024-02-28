402bb3f8ebed170de231dccf088c8f4a81626fb6
Atharv Bhosekar <abhosekar@gams.com>
2024-02-28 15:50:15 -0500

    updated buildignore file for the new api

--- update_buildignore.R
+++ update_buildignore.R
@@ -3,7 +3,7 @@ gdx_ignore = list.files("src/gdx", full.names=TRUE, all.files=TRUE)
 gdx_ignore = gdx_ignore[gdx_ignore != "src/gdx/src"]
 
 gdx_src_all = list.files("src/gdx/src", full.names=TRUE)
-gdx_src_cpp_h = list.files("src/gdx/src", pattern="(*\\.cpp$)|(*\\.h)", full.names=TRUE)
+gdx_src_cpp_h = list.files("src/gdx/src", pattern="(*\\.cpp$)|(*\\.h)|(*\\.o)", full.names=TRUE)
 gdx_license = "src/gdx/LICENSE"
 ignore_list = setdiff(gdx_src_all, gdx_src_cpp_h)
 ignore_list = setdiff(ignore_list, gdx_license)
